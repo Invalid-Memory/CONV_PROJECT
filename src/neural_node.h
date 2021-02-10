9b3be696957cea6980543c00a76b598941e0f765
Invalid-Memory <djy15000296784@163.com>
2021-02-10 23:50:10 +0800

    source

---
+++ src/neural_node.h
@@ -0,0 +1,74 @@
+/*--------------------------------------------------------------------------\
+|   neural_node.h of ConvNeurNet Project---------Student Work               |
+|   Copyright (C) 2021  丁嘉一                                               |
++                                                                           +
+|   This program is free software: you can redistribute it and/or modify    |
+|   it under the terms of the GNU General Public License as published by    |
+|   the Free Software Foundation, either version 3 of the License, or       |
+|   (at your option) any later version.                                     |
++                                                                           +
+|   This program is distributed in the hope that it will be useful,         |
+|   but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+|   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+|   GNU General Public License for more details.                            |
++                                                                           +
+|   You should have received a copy of the GNU General Public License       |
+|   along with this program.  If not, see <https://www.gnu.org/licenses/>.  |
+\--------------------------------------------------------------------------*/
+#ifndef   CONV_PROJECT_NODE
+# define  CONV_PROJECT_NODE
+# include "vector.h"
+# include "string.h"
+ namespace conv{
+     typedef vector<ldouble> ldvector;
+     class node{
+     protected:
+         typedef           node      *pnode          ;
+         ldouble           data                      ;
+         ldouble*          inweight                  ;
+         int               weilen                    ;
+//       pnode*            link                      ;
+//       int               link_num                  ;
+     public:
+         explicit          node      (ldvector&)     ;
+         virtual          ~node      (void)          ;
+         virtual  ldouble  activate  (ldvector&) pure;
+     };
+     class sigmoidnode:public node{
+     public:
+         inline sigmoidnode(ldvector&);
+         ldouble activate(ldvector&)override;
+     };
+     class relunode:public node{
+     public:
+         inline           relunode (ldvector&)                  ;
+         virtual  inline ~relunode (void)      override =default;
+         ldouble          activate (ldvector&) override         ;
+     }                                                          ;
+     class softplusnode:public node{
+     public:
+         explicit inline  softplusnode (ldvector&)                  ;
+         virtual  inline ~softplusnode (void)      override =default;
+         ldouble          activate     (ldvector&) override         ;
+     }                                                              ;
+
+     inline conv::sigmoidnode::sigmoidnode(ldvector& n):node(n){
+     }
+     inline conv::relunode::relunode(ldvector& n):node(n){
+     }
+     inline conv::softplusnode::softplusnode(ldvector& n):node(n){
+     }
+//rubbish bin----------------------------------------------
+     /*class convnode:public node{
+     public:
+         explicit           convnode (void)             ;
+         virtual  inline   ~convnode (void)     =default;
+         ldouble            activate (obj&)    override;
+//       inline   ldouble&  getw     (void)             ;
+     };*/
+     /*inline ldouble& conv::convnode::getw(void){
+         return data;
+     }*/
+//---------------------------------------------------------
+ }
+#endif
---
+++ src/vector.h
@@ -0,0 +1,22 @@
+/*--------------------------------------------------------------------------\
+|   vector.h of ConvNeurNet Project---------Student Work                    |
+|   Copyright (C) 2021  丁嘉一                                               |
++                                                                           +
+|   This program is free software: you can redistribute it and/or modify    |
+|   it under the terms of the GNU General Public License as published by    |
+|   the Free Software Foundation, either version 3 of the License, or       |
+|   (at your option) any later version.                                     |
++                                                                           +
+|   This program is distributed in the hope that it will be useful,         |
+|   but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+|   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+|   GNU General Public License for more details.                            |
++                                                                           +
+|   You should have received a copy of the GNU General Public License       |
+|   along with this program.  If not, see <https://www.gnu.org/licenses/>.  |
+\--------------------------------------------------------------------------*/
+#ifndef   CONV_PROJECT_VECTOR
+# define  CONV_PROJECT_VECTOR
+# include "vector_dec.h"
+# include "vector_def.hpp"
+#endif
